9fa0444d897103d57256fe17fc25f83af8dcf759
Igor Martynov <igor.martynov@intel.com>
2015-11-06 15:56:18 +0300

    Update package.bat to include vim-pkg directory.

--- package.bat
+++ package.bat
@@ -3,14 +3,14 @@ setlocal EnableExtensions
 setlocal EnableDelayedExpansion
 
 set archive=vimos.7z
-set files=apps workspace README.md
+set files=apps workspace vim-pkg README.md
 
 :: Check if 7-zip is available
-7z.exe >NUL 2>&1
+7z >NUL 2>&1
 if errorlevel 1 (
     echo.7-zip cannot be run. Make sure 7z.exe is in your PATH.
     exit 1
 )
 
-7z.exe a %archive% %files%
+7z a %archive% %files%
 
